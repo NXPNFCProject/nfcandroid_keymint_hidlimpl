908383cae2fe35b424a1146e77123e52ae7a7531
nxf77715 <Shrivatsa.Hande@nxp.com>
2021-12-07 12:45:36 +0530

    Coverity fix
    - Fixed coverity warnings
    - Added License header
    
    Change-Id: I7dabb879fe549f4c9bf8c40ac939353f492267cf

--- keymint/transport/SocketTransport.cpp
+++ keymint/transport/SocketTransport.cpp
@@ -14,6 +14,25 @@
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
  */
+/******************************************************************************
+**
+** The original Work has been changed by NXP.
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+** http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+**
+** Copyright 2021 NXP
+**
+*********************************************************************************/
 #include "SocketTransport.h"
 #include "ITransport.h"
 #include <android-base/logging.h>
@@ -87,8 +106,9 @@ bool SocketTransport::sendData(const vector<uint8_t>& inData, vector<uint8_t>& o
     ssize_t valRead = read(mSocket, buffer, MAX_RECV_BUFFER_SIZE);
     if (0 > valRead) {
         LOG(ERROR) << "Failed to read data from socket.";
+        return false;
     }
-    for (size_t i = 0; i < valRead; i++) {
+    for (size_t i = 0; i < (size_t)valRead; i++) {
         output.push_back(buffer[i]);
     }
     return true;
