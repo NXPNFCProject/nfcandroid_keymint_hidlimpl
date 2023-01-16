9ecdb6e4a99bb7d48d00102965c33cd1c297f497
nxf77715 <shrivatsa.hande@nxp.com>
2023-01-16 20:21:43 +0530

    Keymint Reference Hal implementation taken from github.
    - 1. Ignore SIGPIPE signal and receive EPIPE error code.
    
    Change-Id: Ia807bd7a19d802d70243fa17f27ed1e0e8897a92

--- keymint/transport/SocketTransport.cpp
+++ keymint/transport/SocketTransport.cpp
@@ -78,9 +78,9 @@ bool SocketTransport::sendData(const vector<uint8_t>& inData, vector<uint8_t>& o
     inDataPrependedLength.push_back(static_cast<uint8_t>(inData.size() & 0xFF));
     inDataPrependedLength.insert(inDataPrependedLength.end(), inData.begin(), inData.end());
 
-    if (0 > send(mSocket, inDataPrependedLength.data(), inDataPrependedLength.size(), 0)) {
+    if (0 > send(mSocket, inDataPrependedLength.data(), inDataPrependedLength.size(), MSG_NOSIGNAL)) {
         static int connectionResetCnt = 0; /* To avoid loop */
-        if (ECONNRESET == errno && connectionResetCnt == 0) {
+        if ((ECONNRESET == errno || EPIPE == errno) && connectionResetCnt == 0) {
             // Connection reset. Try open socket and then sendData.
             socketStatus = false;
             connectionResetCnt++;
