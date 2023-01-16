dc236c04652451dd2c918e69807b724461f2dbc7
nxf77715 <shrivatsa.hande@nxp.com>
2023-01-16 20:08:41 +0530

    Keymint Reference Hal implementation taken from github.
    - Judgement fall into failure case.
    
    Change-Id: Id056a19fe60e543a9bea74c10b296e63cdf7734a

--- keymint/transport/OmapiTransport.cpp
+++ keymint/transport/OmapiTransport.cpp
@@ -209,8 +209,8 @@ bool OmapiTransport::internalTransmitApdu(
     }
 
     if ((selectResponse.size() < 2)
-        || ((selectResponse[selectResponse.size() -1] & 0xFF) == 0x00)
-        || ((selectResponse[selectResponse.size() -2] & 0xFF) == 0x90))
+        || ((selectResponse[selectResponse.size() -1] & 0xFF) != 0x00)
+        || ((selectResponse[selectResponse.size() -2] & 0xFF) != 0x90))
     {
         LOG(ERROR) << "Failed to select the Applet.";
         return false;
@@ -371,8 +371,8 @@ bool OmapiTransport::internalProtectedTransmitApdu(
         return false;
       }
       if ((selectResponse.size() < 2)
-          || ((selectResponse[selectResponse.size() -1] & 0xFF) == 0x00)
-          || ((selectResponse[selectResponse.size() -2] & 0xFF) == 0x90))
+          || ((selectResponse[selectResponse.size() -1] & 0xFF) != 0x00)
+          || ((selectResponse[selectResponse.size() -2] & 0xFF) != 0x90))
       {
           LOG(ERROR) << "Failed to select the Applet.";
           return false;
