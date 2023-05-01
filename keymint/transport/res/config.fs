aca36b6c5528fe12cf905f40a8eee2905d205c14
suryaprakash.konduru <suryaprakash.konduru@nxp.com>
2023-05-01 18:53:44 +0530

    Add NXP suffix
    Avoid conflicts with other HAL implementations.
    
    Bug: 270239534
    Test: m
    Change-Id: If7014ccb4c628d87c15676cac66c127f05fe6aaf

--- keymint/transport/res/config.fs
+++ keymint/transport/res/config.fs
@@ -7,13 +7,13 @@ value:2902
 [AID_VENDOR_NXP_AUTHSECRET]
 value:2903
 
-[vendor/bin/hw/android.hardware.security.keymint-service.strongbox]
+[vendor/bin/hw/android.hardware.security.keymint-service.strongbox.nxp]
 mode: 0755
 user: AID_VENDOR_NXP_STRONGBOX
 group: AID_SYSTEM
 caps: SYS_ADMIN SYS_NICE WAKE_ALARM
 
-[vendor/bin/hw/android.hardware.weaver@1.0-service]
+[vendor/bin/hw/android.hardware.weaver@1.0-service.nxp]
 mode: 0755
 user: AID_VENDOR_NXP_WEAVER
 group: AID_SYSTEM
