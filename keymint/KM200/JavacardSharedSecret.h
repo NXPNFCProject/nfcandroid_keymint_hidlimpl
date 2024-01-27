5595b6cbc08726f0817cc7d2091dd8d594ff8cf8
nxf56319 <sai.shwethas@nxp.com>
2024-01-27 20:59:40 +0530

    Added memory leak trace for KeyMint HAL
    Added support to print the memory leak trace
    as part of dump method
    
    Change-Id: I9a5d2913436feddd3f62e95260c47170615339d9

--- keymint/KM200/JavacardSharedSecret.h
+++ keymint/KM200/JavacardSharedSecret.h
@@ -1,3 +1,22 @@
+/******************************************************************************
+ *
+ *  The original Work has been changed by NXP.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Copyright 2022 NXP
+ *
+ ******************************************************************************/
 #pragma once
 
 #include "CborConverter.h"
@@ -19,6 +38,9 @@ class JavacardSharedSecret : public BnSharedSecret {
   public:
     explicit JavacardSharedSecret(shared_ptr<JavacardSecureElement> card) : card_(card) {}
     virtual ~JavacardSharedSecret() {}
+
+    // Methods from ::ndk::ICInterface follow.
+    binder_status_t dump(int fd, const char** args, uint32_t num_args) override;
 
     ScopedAStatus getSharedSecretParameters(SharedSecretParameters* params) override;
 
