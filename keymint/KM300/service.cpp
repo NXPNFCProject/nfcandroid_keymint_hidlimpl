623111affa735ac9821de2b5f0ae5b35120620a7
nxf63690 <sourav.kumar@nxp.com>
2024-05-30 14:43:38 +0530

    Copyright banner year update

--- keymint/KM300/service.cpp
+++ keymint/KM300/service.cpp
@@ -29,7 +29,7 @@
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
  **
- ** Copyright 2020-2023 NXP
+ ** Copyright 2020-2024 NXP
  **
  *********************************************************************************/
 #define LOG_TAG "javacard.strongbox-service"
