0964a5bef7918844c45400da3e1496b368b85145
nxf45198 <fahim.akhtar@nxp.com>
2025-03-11 14:06:29 +0530

    MW Version add for IAR module
    -  NXP NFC MW Versioning added for IAR module

--- keymint/KM_Common/authsecret/service.cpp
+++ keymint/KM_Common/authsecret/service.cpp
@@ -42,7 +42,45 @@
 
 using ::aidl::android::hardware::authsecret::AuthSecret;
 
+#define NXP_EN_SN110U 1
+#define NXP_EN_SN100U 1
+#define NXP_EN_SN220U 1
+#define NXP_EN_PN557 1
+#define NXP_EN_PN560 1
+#define NXP_EN_SN300U 1
+#define NXP_EN_SN330U 1
+#define NFC_NXP_MW_ANDROID_VER (16U)  /* Android version used by NFC MW */
+#define NFC_NXP_MW_VERSION_MAJ (0x04) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
+#define NFC_NXP_MW_CUSTOMER_ID (0x00) /* MW Customer Id */
+#define NFC_NXP_MW_RC_VERSION (0x00)  /* MW RC Version */
+
+/******************************************************************************
+ * Function         printIARVersion
+ *
+ * Description      This function is called to print the IAR version
+ *
+ * Returns          None
+ *
+ ******************************************************************************/
+static void printIARVersion() {
+    uint32_t validation = (NXP_EN_SN100U << 13);
+    validation |= (NXP_EN_SN110U << 14);
+    validation |= (NXP_EN_SN220U << 15);
+    validation |= (NXP_EN_PN560 << 16);
+    validation |= (NXP_EN_SN300U << 17);
+    validation |= (NXP_EN_SN330U << 18);
+    validation |= (NXP_EN_PN557 << 11);
+
+    char version[60];  // Buffer to store formatted string
+    sprintf(version, "IAR Version: NFC_AR_%02X_%05X_%02d.%02X.%02X",
+            NFC_NXP_MW_CUSTOMER_ID, validation, NFC_NXP_MW_ANDROID_VER,
+            NFC_NXP_MW_VERSION_MAJ, NFC_NXP_MW_VERSION_MIN);
+    LOG(INFO) << version;
+}
+
 int main() {
+	printIARVersion();
     ABinderProcess_setThreadPoolMaxThreadCount(0);
     std::shared_ptr<AuthSecret> authsecret = ndk::SharedRefBase::make<AuthSecret>();
 
