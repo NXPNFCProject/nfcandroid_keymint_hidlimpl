3a35e20980e7fb375d596b2d30f95090aede4735
nxf56319 <sai.shwethas@nxp.com>
2025-08-14 17:59:20 +0530

    Updated MW version with 16.08.00_TC for early patch drop

--- keymint/KM_Common/KeyMint4Service.cpp
+++ keymint/KM_Common/KeyMint4Service.cpp
@@ -64,7 +64,7 @@
 #define NXP_EN_SN300U 1
 #define NXP_EN_SN330U 1
 #define NFC_NXP_MW_ANDROID_VER (16U)  /* Android version used by NFC MW */
-#define NFC_NXP_MW_VERSION_MAJ (0x07) /* MW Major Version */
+#define NFC_NXP_MW_VERSION_MAJ (0x08) /* MW Major Version */
 #define NFC_NXP_MW_VERSION_MIN (0x00) /* MW Minor Version */
 #define NFC_NXP_MW_CUSTOMER_ID (0x00) /* MW Customer Id */
 #define NFC_NXP_MW_RC_VERSION (0x00)  /* MW RC Version */
@@ -109,7 +109,7 @@ static void printKeyMint4Version() {
   validation |= (NXP_EN_PN557 << 11);
 
   char version[60];  // Buffer to store formatted string
-  sprintf(version, "KEY MINT 4 Version: NFC_AR_%02X_%05X_%02d.%02X.%02X",
+  sprintf(version, "KEY MINT 4 Version: NFC_AR_%02X_%05X_%02d.%02X.%02X_TC",
           NFC_NXP_MW_CUSTOMER_ID, validation, NFC_NXP_MW_ANDROID_VER,
           NFC_NXP_MW_VERSION_MAJ, NFC_NXP_MW_VERSION_MIN);
   LOG(INFO) << version;
