813d87157058364a2d22556d6d80166e2730b21e
nxf97491 <ramyesh.thanthri@nxp.com>
2025-08-28 16:18:11 +0530

    Update copyright year

--- keymint/transport/include/HalToHalTransport.h
+++ keymint/transport/include/HalToHalTransport.h
@@ -30,7 +30,7 @@
  ** See the License for the specific language governing permissions and
  ** limitations under the License.
  **
- ** Copyright 2020-2021 NXP
+ ** Copyright 2020-2021, 2025 NXP
  **
  *********************************************************************************/
 #pragma once
