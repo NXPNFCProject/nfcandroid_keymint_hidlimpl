a865cd0e4f521cd027074f6adfd76d80d17e5d72
Sourav Kumar <sourav.kumar@nxp.com>
2026-03-23 15:10:59 +0530

    Refine the importWrappedKey function implementation
    This commit refines the importWrappedKey function to correctly
    associate user security identifiers with imported keys.
    When a key's authorization list specifies user_auth_type as
    HWAuthType::PASSWORD, passwordSid is added to the authorization
    list as USER_SECURE_ID. Similarly, if the user_auth_type is
    HWAuthType::FINGERPRINT, biometricSid is added as USER_SECURE_ID.
    However, if the user_auth_type contains HWAuthType::ANY, passwordSid
    is added as USER_SECURE_ID.This ensures that imported keys are properly
    bound to the user's security credentials, adhering to the specified
    security requirements.
    
    Change-Id: I0c6588921472b8c8f238fcd395934f6de0d39119

--- keymint/HAL/JavacardKeyMintDevice.cpp
+++ keymint/HAL/JavacardKeyMintDevice.cpp
@@ -29,7 +29,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- *  Copyright 2022,2024-2025 NXP
+ *  Copyright 2022,2024-2025, 2026 NXP
  *
  ******************************************************************************/
 #include "JavacardKeyMintDevice.h"
@@ -46,7 +46,9 @@
 
 namespace keymint::javacard {
 using aidl::android::hardware::security::keymint::BufferingMode;
+using aidl::android::hardware::security::keymint::HardwareAuthenticatorType;
 using aidl::android::hardware::security::keymint::JavacardKeyMintOperation;
+using aidl::android::hardware::security::keymint::KeyParameterValue;
 using aidl::android::hardware::security::keymint::Tag;
 namespace km_utils = ::aidl::android::hardware::security::keymint::km_utils;
 using cppbor::EncodedItem;
@@ -57,6 +59,23 @@ using ::keymaster::KeymasterBlob;
 using ::keymaster::KeymasterKeyBlob;
 using ::keymint::javacard::Instruction;
 using std::string;
+
+namespace {
+std::optional<vector<KeyParameter>::iterator> findTag(Tag tag, vector<KeyParameter>& params) {
+    auto it = std::find_if(params.begin(), params.end(),
+                           [tag](const auto& param) { return param.tag == tag; });
+
+    if (it != params.end()) {
+        return it;  // Found, return the iterator
+    }
+    return std::nullopt;  // Not found
+}
+
+HardwareAuthenticatorType hwAuthTypeFromUserSecureId(const KeyParameterValue& userSecureId) {
+    const auto longInteger = userSecureId.get<KeyParameterValue::longInteger>();
+    return static_cast<HardwareAuthenticatorType>(0xFFFFFFFF & longInteger);
+}
+}  // namespace
 
 ScopedAStatus JavacardKeyMintDevice::defaultHwInfo(KeyMintHardwareInfo* info) {
     info->versionNumber = version_;
@@ -195,6 +214,27 @@ ScopedAStatus JavacardKeyMintDevice::importWrappedKey(const vector<uint8_t>& wra
     if (errorCode != KM_ERROR_OK) {
         LOG(ERROR) << "Error in send begin import wrapped key in importWrappedKey.";
         return km_utils::kmError2ScopedAStatus(errorCode);
+    }
+
+    auto optionalItr = findTag(Tag::USER_SECURE_ID, authList);
+    if (optionalItr) {
+        // If both the Password and Fingerprint bits are set in UserSecureId, the password SID
+        // should be used, because biometric auth tokens contain both password and fingerprint
+        // SIDs, but password auth tokens only contain the password SID.
+        std::vector<KeyParameter>::iterator it = *optionalItr;
+        const auto hwAuthType = hwAuthTypeFromUserSecureId(it->value);
+
+        long sid = 0;
+        if (hwAuthType == HardwareAuthenticatorType::ANY ||
+            hwAuthType == HardwareAuthenticatorType::PASSWORD) {
+            sid = passwordSid;
+        } else if (hwAuthType == HardwareAuthenticatorType::FINGERPRINT) {
+            sid = biometricSid;
+        }
+
+        if (sid != 0) {
+            it->value = KeyParameterValue::make<KeyParameterValue::longInteger>(sid);
+        }
     }
     // Finish the import
     std::tie(item, errorCode) = sendFinishImportWrappedKeyCmd(
