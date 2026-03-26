a056c179fedcbea0e84962c53794e8ce31249dc1
nxf97491 <ramyesh.thanthri@nxp.com>
2026-03-26 17:59:46 +0530

    MW version updated to 17.05.00
    Change-Id: I2ea838dcc0f370422ce9cacb65b959a9be490e87

--- keymint/HAL/KeyMint3Service.cpp
+++ keymint/HAL/KeyMint3Service.cpp
@@ -110,7 +110,7 @@ static void printKeyMint3Version() {
   validation |= (NXP_EN_PN557 << 11);
 
   char version[60];  // Buffer to store formatted string
-  sprintf(version, "KEY MINT 3 Version: NFC_AR_%02X_%05X_%02u.%02X.%02X_TC5",
+  sprintf(version, "KEY MINT 3 Version: NFC_AR_%02X_%05X_%02u.%02X.%02X",
           NFC_NXP_MW_CUSTOMER_ID, validation, NFC_NXP_MW_ANDROID_VER, NFC_NXP_MW_VERSION_MAJ,
           NFC_NXP_MW_VERSION_MIN);
   LOG(INFO) << version;
